05ee421ba2cfab63fe09eaaa402d1a8aee31b2e3
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2024-05-09 21:17:53 +0530

    project id added

--- variables.tf
+++ variables.tf
@@ -17,3 +17,11 @@ variable "maxNode" {
 variable "machineType" {
   description = "Node Instance machine type"
 }
+
+variable "project_id" {
+  default = "mytesting-400910"
+}
+
+variable "region" {
+  default = "us-central1"
+}
